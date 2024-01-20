c074d45d0ec554300ed6e583503e84746acbf1fa
f3rn0s <finn@volkis.com.au>
2024-01-20 15:58:56 +1000

    Clean up get_forest_level function

--- src/enums/forestlevel.rs
+++ src/enums/forestlevel.rs
@@ -2,14 +2,14 @@
 pub fn get_forest_level(level: String) -> String
 {
     match level.as_str() {
-        "7" => { return "2016".to_string(); },
-        "6" => { return "2012 R2".to_string(); },
-        "5" => { return "2012".to_string(); },
-        "4" => { return "2008 R2".to_string(); },
-        "3" => { return "2008".to_string(); },
-        "2" => { return "2003".to_string(); },
-        "1" => { return "2003 Interim".to_string(); },
-        "0" => { return "2000 Mixed/Native".to_string(); },
-        _ => { return "Unknown".to_string(); },
-    }
+        "7" => { "2016" },
+        "6" => { "2012 R2" },
+        "5" => { "2012" },
+        "4" => { "2008 R2" },
+        "3" => { "2008" },
+        "2" => { "2003" },
+        "1" => { "2003 Interim" },
+        "0" => { "2000 Mixed/Native" },
+        _   => { "Unknown" },
+    }.to_string()
 }
