afe234a27f07133ab5ae3b15b50cb1aabc7e98ca
f3rn0s <finn@volkis.com.au>
2024-01-20 18:08:09 +1000

    Fixed test snippets in rustdoc so cargo test passes

--- src/objects/mod.rs
+++ src/objects/mod.rs
@@ -3,13 +3,20 @@
 //! Example in rust:
 //!
 //! ```rust
+//! # use rusthound::objects::user::User;
+//! # use rusthound::objects::group::Group;
+//! # use rusthound::objects::computer::Computer;
+//! # use rusthound::objects::ou::Ou;
+//! # use rusthound::objects::gpo::Gpo;
+//! # use rusthound::objects::domain::Domain;
+//! # use rusthound::objects::container::Container;
 //! let user = User::new();
-//! let group = Groupr::new();
-//! let computer = Computerr::new();
-//! let ou = Our::new();
-//! let gpo = Gpor::new();
-//! let domain = Domainr::new();
-//! let container = Containerr::new();
+//! let group = Group::new();
+//! let computer = Computer::new();
+//! let ou = Ou::new();
+//! let gpo = Gpo::new();
+//! let domain = Domain::new();
+//! let container = Container::new();
 //! ```
 //! 
 pub mod user;
